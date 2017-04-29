76d855ebda00a60fc441f6585661fa84e4b578be
Zhaowei Xu <zhaoweix@usc.edu>
2017-04-28 23:39:17 -0700

    Update views.py

--- qaq/views.py
+++ qaq/views.py
@@ -182,10 +182,14 @@ def googleEntityAnalysis(sentence):
 # 		});
 
 @csrf_exempt
-def up_data(d):
+def up_data():
+	with open('voceler-8d934-export.json') as data_file:
+		d = json.load(data_file)
 	questions = d['Questions-v1']
 	for key,value in questions:
 		q = Question(qid = key, qTags = '', qTime = int(round(time.time() * 10)))
+		q.save()
+		print(q.qid)
 @csrf_exempt		
 def add_ques(d):
 	#question = d['qDescription']
@@ -338,8 +342,10 @@ def conclude(d):
 
 @csrf_exempt
 def index(request):
+	up_data()
 	if request.method != 'POST':
 		return JsonResponse({'Error':'Please use POST request'})
+	
 	d = json.loads(request.body.decode('utf-8'))
 	d = defaultdict(lambda: None, d)
 	action = d['action']
@@ -350,7 +356,7 @@ def index(request):
 	elif action == 'conclude_ques':
 		conclude(d)
 	elif action == 'update_database':
-		up_data(d)
+		up_data()
 	return JsonResponse(
 		{
 			'success':True,
